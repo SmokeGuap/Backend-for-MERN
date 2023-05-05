b6558509832d208aeba6ab1587ce9a4aaabb675b
Kirill <mustang.horse34@gmail.com>
2023-05-05 12:54:30 +0700

    Fixed jwt verify

--- middleWares/checkAuth.js
+++ middleWares/checkAuth.js
@@ -5,19 +5,19 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 export default (req, res, next) => {
   const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
-  if (token) {
+  if (token != 'null') {
     try {
       const decoded = jwt.verify(token, JWT_SECRET);
       req.userId = decoded._id;
       next();
     } catch {
       return res.status(403).json({
-        message: 'Нет доступа',
+        message: 'Не удалось верифицировать токен',
       });
     }
   } else {
-    return res.status(403).json({
-      message: 'Нет доступа',
+    return res.status(204).json({
+      message: 'Нет токена',
       token,
     });
   }
