b17f10e320802d9ff752821151ae090ac231b52d
Kirill <mustang.horse34@gmail.com>
2023-05-14 20:52:36 +0700

    Added API getlastcomment

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,42 +1,33 @@
 import Comment from '../models/Comment.js';
 
 export const getAll = async (req, res) => {
-    try {
-      const comments = await Comment.find().populate({
+  try {
+    const comments = await Comment.find().populate({
+      path: 'author',
+      select: ['fullName', 'avatarUrl'],
+    });
+    res.json(comments.reverse());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Не удалось получить комментарии' });
+  }
+};
+
+export const getLastComments = async (req, res) => {
+  try {
+    const comments = await Comment.find()
+      .populate({
         path: 'author',
         select: ['fullName', 'avatarUrl'],
-      });
-      res.json(comments.reverse());
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Не удалось получить комментарии' });
-    }
-  };
-  
-//   export const getLastTags = async (req, res) => {
-//     try {
-//       const posts = await Post.find()
-//         .populate({
-//           path: 'author',
-//           select: ['fullName', 'avatarUrl', 'createdAt'],
-//         })
-//         .limit(5);
-  
-//       const tags = posts.map((item) => item.tags).flat();
-  
-//       const uniqTags = tags.reduce((acc, item) => {
-//         if (acc.includes(item)) {
-//           return acc;
-//         }
-//         return [...acc, item];
-//       }, []);
-//       res.json(uniqTags.slice(0, 5));
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).json({ message: 'Не удалось получить статьи' });
-//     }
-//   };
-  
+      })
+      .limit(5);
+    res.json(comments.reverse());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Не удалось получить комментарии' });
+  }
+};
+
 //   export const getOne = async (req, res) => {
 //     try {
 //       const postId = req.params.id;
@@ -57,7 +48,7 @@ export const getAll = async (req, res) => {
 //       res.status(500).json({ message: 'Не удалось получить статью' });
 //     }
 //   };
-  
+
 //   export const remove = async (req, res) => {
 //     try {
 //       const postId = req.params.id;
@@ -71,21 +62,21 @@ export const getAll = async (req, res) => {
 //       res.status(500).json({ message: 'Не удалось удалить статью' });
 //     }
 //   };
-  
-  export const create = async (req, res) => {
-    try {
-      const doc = new Comment({
-        text: req.body.text,
-        author: req.userId,
-      });
-      const comment = await doc.save();
-      res.json(comment);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Не удалось создать комментарий' });
-    }
-  };
-  
+
+export const create = async (req, res) => {
+  try {
+    const doc = new Comment({
+      text: req.body.text,
+      author: req.userId,
+    });
+    const comment = await doc.save();
+    res.json(comment);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Не удалось создать комментарий' });
+  }
+};
+
 //   export const update = async (req, res) => {
 //     try {
 //       const postId = req.params.id;
